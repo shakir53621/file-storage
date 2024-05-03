92db8ab24bee64d92a2c01d43226c5f12badcdf4
Shakir <shakir.akgubaev@mail.ru>
2024-05-04 00:32:39 +0300

    feature: rewrote hasher to abstract method

---
+++ app/file_manager/hasher.py
@@ -0,0 +1,23 @@
+import hashlib
+from abc import ABC, abstractmethod
+
+
+class AbstractHasher(ABC):
+    @abstractmethod
+    def hash_str(self, string: str) -> str:
+        raise NotImplementedError
+
+
+class MD5Hasher(AbstractHasher):
+    def hash_str(self, string: str) -> str:
+        return hashlib.md5(string.encode('utf-8')).hexdigest()
+
+
+class SHA256Hasher(AbstractHasher):
+    def hash_str(self, string: str) -> str:
+        return hashlib.sha256(string.encode('utf-8')).hexdigest()
+
+
+class SHA1Hasher(AbstractHasher):
+    def hash_str(self, string: str) -> str:
+        return hashlib.sha1(string.encode('utf-8'))  .hexdigest()
