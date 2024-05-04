4a352e09dabb0eb6e38db9d64eddb69d79be76ad
Shakir <shakir.akgubaev@mail.ru>
2024-05-04 17:18:26 +0300

    refactor: put the delete file logic into the service

--- app/main.py
+++ app/main.py
@@ -1,40 +1,22 @@
-import os
-import shutil
+from contextlib import asynccontextmanager
 
-from pathlib import Path
+from fastapi import FastAPI
 
-from fastapi import FastAPI, UploadFile
-
-from app.create_directory.create_directory import CreateDirectory
-from app.file_hasher.filehasher import FileHasher
-from app.find_delete_file import find_and_delete_file
-
-app = FastAPI()
+from app.constants import STORE_ROOT
+from app.file_manager.directory_manager import DirectoryManager
+from app.routers import router
 
 
-@app.post("/uploadfile")
-async def upload_file(file: UploadFile):
-    create_dir = CreateDirectory()
-    # Можем ли мы сразу хешировать при вызове функции?
-    filehasher = FileHasher(file.filename)
-    file_hash = filehasher.create_hash()
+@asynccontextmanager
+async def lifespan(app: FastAPI):  # noqa
+    directory_manager = DirectoryManager()
+    directory_manager.create_directory(STORE_ROOT)
 
-    create_subdir = create_dir.create_subdirectory(file_hash)
-
-    old_path = Path(f"{create_subdir}/{file.filename}")
-    new_path = Path(f"{create_subdir}/{file_hash}")
-
-    with old_path.open("wb+") as file_ob:
-        shutil.copyfileobj(file.file, file_ob)
-
-    os.rename(old_path, new_path)
-
-    return {"filename": file.filename, "filehash": file_hash, "subdir": create_subdir, "new_path": new_path}
+    print("Application initialized...")
+    yield
+    print("Application closed")
 
 
-@app.delete("/delete_file")
-async def delete_file(file_name: str):
-    dir_path = 'store/'
-    if find_and_delete_file(dir_path, file_name):
-        return {"message": f"Файл '{file_name}' успешно удален"}
-    return {"message": "Файл не найден"}
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(router=router)
--- app/routers.py
+++ app/routers.py
@@ -1,4 +1,3 @@
-import os.path
 from typing import Annotated
 
 from fastapi import APIRouter, UploadFile, Depends
@@ -8,7 +7,7 @@ from app.constants import STORE_ROOT
 from app.file_manager.directory_manager import AbstractDirectoryManager, DirectoryManager
 from app.file_manager.file_manager import AbstractFileManager, FileManager
 from app.file_manager.hasher import AbstractHasher, MD5Hasher
-from app.services import FileCreatorService
+from app.services import FileCreatorService, FileDownloaderService, FileDeleterService
 
 router = APIRouter(
     prefix="/files",
@@ -41,26 +40,40 @@ async def download_file(
         file_manager: Annotated[AbstractFileManager, Depends(FileManager)],
 ) -> FileResponse:
 
-    directory_path = os.path.join(STORE_ROOT, file_name[:2])
-    path_to_file = file_manager.find_path_to_file(directory=directory_path, file_name=file_name)
+    file_service = FileDownloaderService(file_manager)
 
-    return FileResponse(
-        path=path_to_file, filename=file_name,
-        media_type='multipart/form-data'
-    )
+    return file_service.download_file(store_root=STORE_ROOT, file_name=file_name)
 
 
-@router.get("/delete-file")
+@router.delete("/delete-file")
 async def delete_file(
         file_name: str,
         file_manager: Annotated[AbstractFileManager, Depends(FileManager)],
 ):
+    file_service = FileDeleterService(file_manager)
 
-    directory_path = os.path.join(STORE_ROOT, file_name[:2])
-    try:
-        if directory_path:
-            path_to_file = file_manager.find_path_to_file(directory=directory_path, file_name=file_name)
-            os.remove(path_to_file)
-            return {"message": f"Файл {path_to_file} успешно удален"}
-    except FileNotFoundError:
-        return {"message": "Файл не найден"}
+    return file_service.delete_file(store_root=STORE_ROOT, file_name=file_name)
+
+
+"""
+todo:
+    1. Добавить ручку удаления файла
+    2. Отрефакторить ручку download_file (вынести логику в сервис)
+    3. Авторизация на основе БД (поднять базу в докере, настроить .env файл)
+    4. Отдавать JWT токен после авторизации
+    5. Поднять в докере
+"""
+
+"""
+table users:
+    1. user_id
+    2. user_name
+    3. password (hashed)
+
+table files:
+    1. file_id
+    2. user_id
+    3. file_hash
+"""
+
+# 1 | 13 | 4a47a0db6e60853dedfcfdf08a5ca249
