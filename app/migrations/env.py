c3a738f3366c269f260c169087f7a0dc9fd5bec7
Shakir <shakir.akgubaev@mail.ru>
2024-05-05 01:50:38 +0300

    fix: remake alembic init

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -5,12 +5,15 @@ from sqlalchemy import pool
 
 from alembic import context
 
-import app.database.models
+import app.db.models # noqa: F401
+from app.config import settings
+from app.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
+config.set_main_option("sqlalchemy.url", f"{settings.database_url}?async_fallback=True")
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -20,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
