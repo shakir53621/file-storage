98d5a680c78b378adc41633339e5b6a333abf460
Shakir <shakir.akgubaev@mail.ru>
2024-05-05 21:08:20 +0300

    fix: changed passwords to password hash

---
+++ sql/users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users(user_id, user_name, password) VALUES
+    (1, 'admin', '0738a3e2111e114a665d35b24be5f42d1f98700f161633f63fbaa15b4b3103ab'),
+	(2, 'user', 'c7f7278ad455e70505cf6eec61ae5d5154edde78330a7614d63e048d179050f4');
