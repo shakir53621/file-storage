3e3b5824e13da24f40b1f1564c62a70df655c4f7
Shakir <shakir.akgubaev@mail.ru>
2024-05-05 03:23:58 +0300

    feature: add endpoints for getting user by id

--- app/db/router.py
+++ app/db/router.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Query
 
 from app.db.repositories import UsersRepository
 from app.db.schemas import SchemaUsers
+from app.db.models import Users
 
 router = APIRouter(
     prefix="/database",
@@ -15,7 +18,13 @@ async def get_all_users() -> list[SchemaUsers]:
     return await UsersRepository.find_all()
 
 
-@router.post("/file")
+@router.get("/user")
+async def get_user_id(user_id: Annotated[int, Query(ge=1, description="id пользователя")]) -> SchemaUsers | None:
+    """Получить пользователя по id"""
+    return await UsersRepository.find_one_or_none(Users.user_id == user_id)
+
+
+@router.post("/file_user")
 async def post_file():
     # Логика заполнения бд
     pass
