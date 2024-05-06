6b5f85487ecca57e48929018d85deed846c9e538
Shakir <shakir.akgubaev@mail.ru>
2024-05-06 03:03:54 +0300

    feature: add method delete_one_or_none for database

--- app/db/repository.py
+++ app/db/repository.py
@@ -28,10 +28,22 @@ class BaseRepository:
 
         return result.mappings().one_or_none()
 
-    async def delete(self, autocommit: bool = False, *filter_by) -> None:
+    async def delete_all(self, autocommit: bool = False, *filter_by: Any) -> None:
         """Удалить запись из БД"""
 
         query = delete(self._model).filter(*filter_by)
         await self._session.execute(query)
         if autocommit:
+            await self._session.commit()
+
+    async def delete_one_or_none(self, *filter_by: Any) -> None:
+        """Удалить одну запись из БД по фильтру, если она существует"""
+
+        query = select(self._model).filter(*filter_by)
+        result = await self._session.execute(query)
+        instance = result.scalars().one_or_none()
+
+        if instance:
+            delete_query = delete(self._model).where(*filter_by)
+            await self._session.execute(delete_query)
             await self._session.commit()
